96e5eccd968e2e3532a6725217f7ae2ca688e19a
jieunheo <gjekdmlwl@naver.com>
2022-10-20 14:31:48 +0900

    class components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const App = () => {
-  return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
-};
+// const App = () => {
+//   return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
+// };
+
+class App extends React.Component {
+  render() {
+    return  <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
+  };
+}
 
 export default App;
