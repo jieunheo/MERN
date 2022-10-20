746371432aa5abfd88808ec98e5e4a90a5ebdf09
jieunheo <gjekdmlwl@naver.com>
2022-10-20 17:42:19 +0900

    Save entered values

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -3,20 +3,26 @@ import React from 'react';
 import './NewGoal.css';
 
 const NewGoal = ({ onAddGoal }) => {
+  let enteredText = '';
+
   const addGoalHandler = (event) => {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'new goal!'
+      text: enteredText
     }
 
     onAddGoal(newGoal);
   }
 
+  const textChangeHandler = (event) => {
+    enteredText = event.target.value;
+  }
+
   return (
     <form className='new-goal' onSubmit={addGoalHandler}>
-      <input type='text' />
+      <input type='text' onChange={textChangeHandler} />
       <button type='submit'>Add Goal</button>
     </form>
   );
