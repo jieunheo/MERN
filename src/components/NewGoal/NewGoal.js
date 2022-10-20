6a460c23051c1093d53f345a08a4c5f5835dc01d
jieunheo <gjekdmlwl@naver.com>
2022-10-20 15:28:58 +0900

    write addGoalHandler event

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -3,8 +3,19 @@ import React from 'react';
 import './NewGoal.css';
 
 const NewGoal = () => {
+  const addGoalHandler = (event) => {
+    event.preventDefault();
+
+    const newGoal = {
+      id: Math.random().toString(),
+      text: 'new goal!'
+    }
+
+    console.log(newGoal);
+  }
+
   return (
-    <form className='new-goal'>
+    <form className='new-goal' onSubmit={addGoalHandler}>
       <input type='text' />
       <button type='submit'>Add Goal</button>
     </form>
