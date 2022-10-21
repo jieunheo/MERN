e905a294bcf333c2903bdd0716fc48cb29bfea04
jieunheo <gjekdmlwl@naver.com>
2022-10-21 16:18:55 +0900

    add component - UserItem component - UserList component - Users page

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import UserItem from "./UserItem";
+import "./UsersList.css";
+
+const UsersList = ({ users }) => {
+  if (users.length === 0) {
+    // user가 없음
+    return (
+      <div className="center">
+        <h2>No Users Found!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {users.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
