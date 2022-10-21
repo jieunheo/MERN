a0f8d342dd5b1d50dc0df5cf6670850a6a157c1b
jieunheo <gjekdmlwl@naver.com>
2022-10-21 16:32:21 +0900

    App styling and other components - UserItem.css - UserList.css - Avatar.js/Avatar.css - Card.js/Card.css

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import Avatar from "../../shared/components/UI/Avatar";
+import Card from "../../shared/components/UI/Card";
 import './UserItem.css';
 
 const UserItem = ({ id, image, name, placeCount }) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{name}</h2>
-          <h3>{placeCount} {placeCount === 1 ? 'Place' : 'Places'}</h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={image} alt={name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{name}</h2>
+            <h3>{placeCount} {placeCount === 1 ? 'Place' : 'Places'}</h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   )
 }
