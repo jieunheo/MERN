f00bc4b6f38f72418eb4779b91a741134169ffbf
jieunheo <gjekdmlwl@naver.com>
2022-10-21 15:45:02 +0900

    Useing react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
+import Users from './user/pages/Users';
+
+const App = () => {
+  return (
+    <Router>
+      <Route path='/' exact>
+        <Users />
+      </Route>
+      <Redirect to='/' />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Users = () => {
+  return (
+    <div>Users 페이지</div>
+  );
+}
+
+export default Users;
