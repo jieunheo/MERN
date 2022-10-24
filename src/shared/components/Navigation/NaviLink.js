0c6d069b4dd6ac94ba6d38f34de25763abc1e23c
jieunheo <gjekdmlwl@naver.com>
2022-10-24 14:37:38 +0900

    Add the main header

---
+++ src/shared/components/Navigation/NaviLink.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './NaviLink.css';
+
+const NaviLink = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default NaviLink;
---
+++ src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './SideDrawer.css';
+
+const SideDrawer = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default SideDrawer;
