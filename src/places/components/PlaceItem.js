f4468d9e6946fd9a178dc399b06f02a961a06e69
jieunheo <gjekdmlwl@naver.com>
2022-11-01 16:25:55 +0900

    Create a modal and connect it

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,29 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UI/Card";
-
+import Modal from "../../shared/components/UI/Modal";
 import './PlaceItem.css';
 
 const PlaceItem = ({ id, image, title, address, description }) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={image} alt={title} />
-        </div>
-        <div className="place-item__info">
-          <h2>{title}</h2>
-          <h3>{address}</h3>
-          <p>{description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>VIEW ON MAP</Button>
-          <Button to={`/places/${id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+    <>
+      <Modal 
+        show={showMap}
+        onCencel={closeMapHandler}
+        header={address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      />
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={image} alt={title} />
+          </div>
+          <div className="place-item__info">
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button to={`/places/${id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   )
 }
 
