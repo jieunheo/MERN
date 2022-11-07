025b135b7001cb99b3d5035ede3d4e478f1a19d9
jieunheo <gjekdmlwl@naver.com>
2022-11-07 17:53:25 +0900

    Add custom form input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+import Input from "../../shared/components/FormElements/Input";
+import './NewPlace.css';
+
+
 const NewPlace = () => {
   return (
-    <div>NewPlace 페이지</div>
+    <form className="place-form">
+      <Input element='input' type='text' label='Title' />
+    </form>
   );
 }
 
